73488272e355ed3c7282d4fd6e9e29ae2f42198f
dogura <daiki.ogura@soliton.co.jp>
2014-12-24 11:07:15 +0900

    fix some type declare

--- Test.hx
+++ Test.hx
@@ -1,10 +1,37 @@
 package;
 
+// import promisex.Promise;
 import promise.Promise;
 
 
 class Test {
 
+  public static function testPro<T>(?arg: T): Promise<T, String> {
+    return new Promise(function(onFulfilled, onRejected) {
+      if (arg == null) {
+        onRejected('nullじゃん！');
+      }
+      onFulfilled(arg);
+    });
+  }
+
+  public static function asyncLook(): Promise<String, String> {
+    return new Promise(function(ff, rj) {
+      trace('prepre');
+      haxe.Timer.delay(function() ff('heeei!'), 1000);
+      trace('end');
+    });
+  }
+
   public static function main() {
+    testPro('test!')
+      .then(function(v) {trace(v); return 'second';}, function(err) return err)
+      .then(function(v) trace(v), null);
+
+    testPro()
+      .then(function(v) trace(v), function(err) trace(err));
+
+    asyncLook()
+      .then(function(v) trace(v), null);
   }
 }
--- promise/Promise.hx
+++ promise/Promise.hx
@@ -4,20 +4,22 @@ package promise;
 import promise.IThenable;
 
 
-extern class Promise<T> implements IThenable<T> {
+@:native("Promise")
+extern class Promise<T, E> implements IThenable<T> {
 
   public static function all<T>(promises: Array<IThenable<Dynamic>>): IThenable<T>;
   // NOTE: postpone
   // public static function race
   @:overload(function <T>(thenable: {then: Dynamic -> IThenable<T>}): Resolve<T> {})
-  @:overload(function <T>(thenable: Promise<T>): Resolve<T> {})
+  @:overload(function <T, E>(thenable: Promise<T, E>): Promise<T, E> {})
   public static function resolve<T>(obj: T): Resolve<T>;
 
-  public static function reject<U>(obj: U): Reject<U>;
+  public static function reject<E>(obj: E): Reject<E>;
 
-  public function new<T, U>(expr: Resolve<T> -> Reject<U> -> IThenable<T>, label: String='');
+  // public function new<T, E>(expr: Resolve<T> -> Reject<E> -> IThenable<T>, label: String='');
+  public function new<T, E>(expr: (T -> Void) -> (E -> Void) -> Void, label: String='');
 
-  @:overload(function <T, U>(onFulfilled: T -> ThenableType<U>, onRejected: T -> U): IThenable<U> {})
-  @:overload(function <T, U>(onFulfilled: T -> IThenable<U>, onRejected: T -> U): IThenable<U> {})
-  public function then<T, U>(onFulfilled: T -> U, onRejected: T -> U): IThenable<U>;
+  @:overload(function <T, E>(onFulfilled: T -> ThenableType<E>, onRejected: T -> E): IThenable<E> {})
+  @:overload(function <T, E>(onFulfilled: T -> IThenable<E>, onRejected: T -> E): IThenable<E> {})
+  public function then<T, E>(onFulfilled: T -> E, onRejected: T -> E): IThenable<E>;
 }
